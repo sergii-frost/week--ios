8d210273c8d762102582cf29bbac113189f0ed7a
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2016-07-03 14:12:31 +0200

    adding tomorrow, nextWeek, thisWeek, lastWeek helpers

--- weeknum/weeknum/Extensions/DateTools.swift
+++ weeknum/weeknum/Extensions/DateTools.swift
@@ -22,7 +22,22 @@ enum WeekDays: Int {
         case .Sunday:       return "Sun".localized
         }
     }
+    
+    var daysLeftTillNextWeek: Double {
+        switch self {
+        case .Monday:   return 7
+        case .Tuesday:  return 6
+        case .Wednesday:return 5
+        case .Thursday: return 4
+        case .Friday:   return 3
+        case .Saturday: return 2
+        case .Sunday:   return 1
+        }
+    }
 }
+
+let DAY: NSTimeInterval = 60*60*24
+let WEEK: NSTimeInterval = DAY*7
 
 extension NSDate {
     func dayOfWeek() -> Int? {
@@ -48,5 +63,49 @@ extension NSDate {
         df.locale = NSLocale.currentLocale()
         df.dateFormat = dateFormat
         return df.stringFromDate(self)
+    }
+    
+    func tomorrow() -> NSDate? {
+        guard
+            let cal: NSCalendar = NSCalendar.currentCalendar()
+            else { return nil }
+        
+        cal.timeZone = NSTimeZone.systemTimeZone()
+        let startOfDay = cal.startOfDayForDate(self)
+        return startOfDay.dateByAddingTimeInterval(DAY)
+    }
+    
+    func nextWeekStart() -> NSDate? {
+        guard
+            let cal: NSCalendar = NSCalendar.currentCalendar(),
+            let comp: NSDateComponents = cal.components(.Weekday, fromDate: self)
+            else { return nil }
+        
+        let startOfDay = cal.startOfDayForDate(self)
+        guard let weekday = WeekDays(rawValue: comp.weekday) else {
+            return nil
+        }
+        return startOfDay.dateByAddingTimeInterval(DAY*weekday.daysLeftTillNextWeek)
+        
+    }
+    
+    func thisWeekStart() -> NSDate? {
+        guard
+            let cal: NSCalendar = NSCalendar.currentCalendar(),
+            let comp: NSDateComponents = cal.components(.Weekday, fromDate: self)
+            else { return nil }
+        
+        let startOfDay = cal.startOfDayForDate(self)
+        guard let weekday = WeekDays(rawValue: comp.weekday) else {
+            return nil
+        }
+        return startOfDay.dateByAddingTimeInterval(DAY*(weekday.daysLeftTillNextWeek-7))
+    }
+    
+    func lastWeekStart() -> NSDate? {
+        guard let thisWeekStart = self.thisWeekStart() else {
+            return nil
+        }
+        return thisWeekStart.dateByAddingTimeInterval(-WEEK)
     }
 }
