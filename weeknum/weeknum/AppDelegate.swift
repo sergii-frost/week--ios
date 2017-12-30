a9a4ec256fc6d01d6708867374789ad70838c8ac
Sergii Nezdolii <sergii@frost.se>
2017-12-30 14:10:10 +0100

    [ADD] adding silent local notifications for weekn# badge
    Apple has limitation of 64 scheduled notifications. Which is fine for Week# app, as we need to schedule max 53 recurring notifications.
    
    Currently silent local notifications are scheduled to occur every week first day of week at midnight.

--- weeknum/weeknum/AppDelegate.swift
+++ weeknum/weeknum/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -35,12 +36,100 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        updateAppBadge(application)
+        checkNotificationStatus()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
+//MARK: - Local notifications and badge update
+extension AppDelegate {
+    
+    //MARK: - Helper methods
+    
+    fileprivate func updateAppBadge(_ application: UIApplication) {
+        guard let weekNumber = Date().weekNumber() else {
+            return
+        }
+        application.applicationIconBadgeNumber = weekNumber
+    }
+    
+    fileprivate func checkNotificationStatus() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .notDetermined:
+                self.askForNotificationAuthorization()
+            case .authorized:
+                self.scheduleBadgeUpdateNotifications()
+            case .denied:
+                self.clearScheduledNotifications()
+            }
+        }
+    }
+    
+    fileprivate func askForNotificationAuthorization() {
+        let alertController = UIAlertController(
+            title: "Wanna badge?",
+            message: "Hey, do you want to see current week # as badge on the app icon? \nI need you to allow app to do that :) \nPS: You can always change it in Settings", preferredStyle: .alert)
+        alertController.view.tintColor = UIColor.weekNumMainColor()
+        alertController.addAction(UIAlertAction(title: "Nah", style: .cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Yeah, sure!", style: .default, handler: { [weak self] _ in
+            self?.requestBadgeAuthorization()
+        }))
+        UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: true, completion: nil)
+    }
+    
+    fileprivate func requestBadgeAuthorization() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.badge]) { (granted: Bool, error: Error?) in
+            if let error = error {
+                print("Error when requesting authorization for badge: \(error.localizedDescription)")
+            }
+            if granted {
+                self.scheduleBadgeUpdateNotifications()
+            }
+        }
+    }
+    
+    
+    /// This function allows to schedule recurring silent local notifications
+    /// in order to update badge number on app icon.
+    /// There are limitations from Apple side for amount of local notifications
+    /// scheduled from one app - up to 64. Recurring notification is counted as one.
+    /// But it should be OK in our case as we have max 53 weeks in the year.
+    /// So current function will shedule 53 recurring notifications, one for each week. 
+    fileprivate func scheduleBadgeUpdateNotifications() {
+        clearScheduledNotifications()
+        guard let weeksRange = Date().weeksRangeInYear() else {
+            return
+        }
+        for week in weeksRange {
+            var dateComponents = DateComponents()
+            dateComponents.weekOfYear = week
+            dateComponents.weekday = Calendar.current.firstWeekday
+            dateComponents.hour = 0
+            dateComponents.minute = 0
+            dateComponents.second = 0
+            let content = UNMutableNotificationContent()
+            content.badge = NSNumber(value: week)
+            UNUserNotificationCenter.current().add(
+            UNNotificationRequest(
+                identifier: "Week# \(week)",
+                content: content,
+                trigger: UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            )) { error in
+                if let error = error {
+                    print("Error scheduling local notification: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    fileprivate func clearScheduledNotifications() {
+        let userNotificationCenter = UNUserNotificationCenter.current()
+        userNotificationCenter.removeAllDeliveredNotifications()
+        userNotificationCenter.removeAllPendingNotificationRequests()
+    }
+}
